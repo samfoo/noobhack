69c9717629d5e39d67171f15e8f2245a504026fc
Sam Gibson <sam.gibson@socrata.com>
2010-11-14 22:57:26 +0800

    Various refactors.
    * Removed the client code that's not longer used.
    * Delete usless "pass" in __init__.py
    * Refactor run logic to class
    * Support a toggle key to switch between game mode and help mode.
    * Fix up some pylint complaints in proxy.py

--- noobhack.py
+++ noobhack.py
@@ -16,130 +16,176 @@ import telnet
 import process
 import proxy
 
-def usage():
-    sys.stderr.write("""Usage: noobhack.py [options]
-Help: noobhack helps you ascend in nethack.
-    By default, it runs a copy of nethack locally, however it's possible to
-    connect to a remote telnet server and proxy a game.
-Options:
-    -l      Play a local game (default)
-    -h      Host to play a remote game on
-    -p      Port to connect to the remote host (default: 23)""")
-    sys.exit(1)
+class Noobhack:
+    toggle_key = "\t"
 
-def parse_options():
-    opts_dict = {"local": True}
+    def __init__(self):
+        self.options = self.parse_options()
+        self.mode = "game"
 
-    options, remaining = \
-        getopt.getopt(sys.argv[1:], "lh:p:", [])
+        self.game = self.connect_to_game() 
+        self.output_proxy = proxy.Output(self.game)
+        self.input_proxy = proxy.Input(self.game) 
 
-    if len(remaining) > 0:
-        sys.stderr.write("noobhack: unrecognized argument(s) `%s'\n" % \
-                         ",".join(remaining))
-        usage()
+        # Create an in-memory terminal screen and register it's stream
+        # processor with the output proxy.
+        self.stream = vt102.stream()
 
-    opts = [pair[0] for pair in options]
-    if ("-h" in opts and "-l" in opts) or ("-p" in opts and "-l" in opts):
-        sys.stderr.write("noobhack: invalid option `-p' or `-h' with `-l'")
-        usage()
+        # For some reason that I can't assertain: curses freaks out and crashes
+        # when you use exactly the number of rows that are available on the
+        # terminal. It seems easiest just to subtract one from the rows and 
+        # deal with it rather than hunt forever trying to figure out what I'm
+        # doing wrong with curses.
+        rows, cols = self.size()
+        self.term = vt102.screen((rows-1, cols))
+        self.term.attach(self.stream)
+        self.output_proxy.register(self.stream.process)
 
-    if "-p" in opts and "-h" not in opts:
-        sys.stderr.write("noobhack: invalid option `-p' requires a host\n")
-        usage()
+        # Register the tilde '~' key to open up the interactive nooback 
+        # assistant.
+        self.input_proxy.register(self._toggle_handler)
 
-    for opt, val in options:
-        if opt == "-h":
-            options["host"] = val
-            options["local"] = False
-        elif opt == "-p":
-            options["port"] = val
+    def usage(self):
+        sys.stderr.write("""Usage: noobhack.py [options]
+    Help: noobhack helps you ascend in nethack.
+        By default, it runs a copy of nethack locally, however it's possible to
+        connect to a remote telnet server and proxy a game.
+    Options:
+        -l      Play a local game (default)
+        -h      Host to play a remote game on
+        -p      Port to connect to the remote host (default: 23)""")
+        sys.stderr.flush()
+        sys.exit(1)
 
-    return opts_dict 
+    def parse_options(self):
+        opts_dict = {"local": True}
 
-def connect_to_game(options):
-    """
-    Fork the game, or connect to a foreign host to play.
+        options, remaining = \
+            getopt.getopt(sys.argv[1:], "lh:p:", [])
 
-    :return: A file like object of the game. Reading/writing is the same as
-    accessing stdout/stdin in the game respectively.
-    """
+        if len(remaining) > 0:
+            sys.stderr.write("noobhack: unrecognized argument(s) `%s'\n" % \
+                             ",".join(remaining))
+            self.usage()
 
-    try:
-        if options.get("local", False):
-            conn = process.Local()
+        opts = [pair[0] for pair in options]
+        if ("-h" in opts and "-l" in opts) or ("-p" in opts and "-l" in opts):
+            sys.stderr.write("noobhack: invalid option `-p' or `-h' with `-l'")
+            self.usage()
+
+        if "-p" in opts and "-h" not in opts:
+            sys.stderr.write("noobhack: invalid option `-p' requires a host\n")
+            self.usage()
+
+        for opt, val in options:
+            if opt == "-h":
+                options["host"] = val
+                options["local"] = False
+            elif opt == "-p":
+                options["port"] = val
+
+        return opts_dict 
+
+    def connect_to_game(self):
+        """
+        Fork the game, or connect to a foreign host to play.
+
+        :return: A file like object of the game. Reading/writing is the same as
+        accessing stdout/stdin in the game respectively.
+        """
+
+        try:
+            if self.options.get("local", False):
+                conn = process.Local()
+            else:
+                conn = telnet.Telnet(
+                    self.options["host"], 
+                    self.options.get("port", 23)
+                )
+            conn.open()
+        except IOError, error:
+            sys.stderr.write("Unable to open nethack: `%s'\n" % error)
+            raise 
+
+        return conn
+
+    def size(self):
+        """
+        Get the current terminal size.
+
+        :return: (rows, cols)
+        """
+
+        raw = fcntl.ioctl(sys.stdin, termios.TIOCGWINSZ, 'SSSS')
+        return struct.unpack('hh', raw) 
+
+    def _toggle_handler(self, key):
+        if key == self.toggle_key:
+            self._toggle_mode()
+
+    def _toggle_mode(self):
+        if self.mode == "game":
+            self.mode = "help"
         else:
-            conn = telnet.Telnet(options["host"], options.get("port", 23))
-        conn.open()
-    except IOError, error:
-        sys.stderr.write("Unable to open nethack: `%s'\n" % error)
-        raise 
+            self.mode = "game"
 
-    return conn
-
-def get_size():
-    """
-    Get the current terminal size.
-
-    :return: (rows, cols)
-    """
-
-    raw = fcntl.ioctl(sys.stdin, termios.TIOCGWINSZ, 'SSSS')
-    return struct.unpack('hh', raw) 
-
-def run(window):
-    options = parse_options()
-
-    # We prefer to let the console pick the colors for the bg/fg instead of
-    # using what curses thinks looks good.
-    curses.use_default_colors()
-
-    game = connect_to_game(options) 
-    output_proxy, input_proxy = proxy.Output(game), proxy.Input(game) 
-
-    # Create an in-memory terminal screen and register it's stream
-    # processor with the output proxy.
-    term_stream = vt102.stream()
-
-    # For some reason that I can't assertain: curses freaks out and crashes
-    # when you use exactly the number of rows that are available on the
-    # terminal. It seems easiest just to subtract one from the rows and 
-    # deal with it rather than hunt forever trying to figure out what I'm
-    # doing wrong with curses.
-    rows, cols = get_size()
-    term_screen = vt102.screen((rows-1, cols))
-    term_screen.attach(term_stream)
-    output_proxy.register(term_stream.process)
-
-    while True:
-        # Let's wait until we have something to do...
-        available = select.select([game.fileno(), sys.stdin.fileno()], [], [])[0]
-
-        if game.fileno() in available:
-            # Do our display logic.
-            output_proxy.proxy()
-
-        if sys.stdin.fileno() in available:
-            # Do our input logic.
-            input_proxy.proxy()
-
+    def _redraw_game(self, window):
         # Repaint the screen with the new contents of our terminal 
         # emulator...
         window.clear()
-        for row_index, game_row in enumerate(term_screen.display):
+        for row_index, game_row in enumerate(self.term.display):
             window.addstr(row_index, 0, game_row)
 
         # Don't forget to move the cursor to where it is in game...
-        cur_x, cur_y = term_screen.cursor()
+        cur_x, cur_y = self.term.cursor()
         window.move(cur_y, cur_x)
 
         # Finally, redraw the whole thing.
         window.refresh()
 
-def main():
-    try:
-        curses.wrapper(run)
-    except IOError, exit_message:
-        print exit_message
+    def _iterate_game(self, window):
+        self._redraw_game(window) 
+
+        # Let's wait until we have something to do...
+        available = select.select(
+            [self.game.fileno(), sys.stdin.fileno()], [], []
+        )[0]
+
+        if self.game.fileno() in available:
+            # Do our display logic.
+            self.output_proxy.proxy()
+
+        if sys.stdin.fileno() in available:
+            # Do our input logic.
+            self.input_proxy.proxy()
+
+    def _redraw_help(self, window):
+        window.clear()
+        window.addstr(0, 0, "help")
+        window.refresh()
+
+    def _iterate_help(self, window):
+        self._redraw_help(window)
+        select.select([sys.stdin.fileno()], [], [])
+        self._toggle_handler(sys.stdin.read(1))
+
+    def run(self, window):
+        # We prefer to let the console pick the colors for the bg/fg instead of
+        # using what curses thinks looks good.
+        curses.use_default_colors()
+
+        while True:
+            if self.mode == "game":
+                self._iterate_game(window)
+            else:
+                self._iterate_help(window)
+
+    def main(self):
+        try:
+            curses.wrapper(self.run)
+        except IOError, exit_message:
+            print exit_message
 
 if __name__ == "__main__":
-    main()
+    hack = Noobhack()
+    hack.main()
--- proxy.py
+++ proxy.py
@@ -1,9 +1,9 @@
-import re
+"""
+Proxies act as the middle-men between the game that's running in a pty and the
+interface that's running in the actual terminal.
+"""
+
 import sys
-import tty
-import socket
-import termios
-import threading
 
 class Input:
     """
@@ -12,24 +12,38 @@ class Input:
     the input is not forwarded to the game.
     """
 
-    def __init__(self, conn):
-        self.conn = conn
+    def __init__(self, game):
+        self.game = game
         self.callbacks = set()
 
     def register(self, callback):
+        """
+        Register a function that will be called *before* any input read off of
+        stdin is forwarded to the game. The callback should return `False` if 
+        it's not to forward the input to the game.
+        """
         self.callbacks.add(callback)
 
     def unregister(self, callback):
+        """
+        Unregister a callback.
+        """
         self.callbacks.remove(callback)
 
     def proxy(self):
-        input = sys.stdin.read(1)
+        """
+        Read a single input character from stdin and, if none of the callbacks
+        return `False`, forward the keystroke to the game. It's the
+        responsibility of the caller to make sure that reading from stdin won't
+        block (e.g. by select or setting it to non-blocking).
+        """
+        key = sys.stdin.read(1)
 
         for callback in self.callbacks:
-            if callback(input) is False:
+            if callback(key) is False:
                 return
 
-        self.conn.write(input)
+        self.game.write(key)
 
 class Output:
     """
@@ -38,18 +52,32 @@ class Output:
     emulator, or something that can parse the output as meaningful to this.
     """
 
-    def __init__(self, conn):
-        self.conn = conn
+    def __init__(self, game):
+        self.game = game
         self.callbacks = set() 
 
     def register(self, callback):
+        """
+        Register a function that will be called whenever any input is read. The
+        function should accept one argument, which will be the data read from 
+        the game.
+        """
         self.callbacks.add(callback)
 
     def unregister(self, callback):
+        """
+        Unregister a callback.
+        """
         self.callbacks.remove(callback)
 
     def proxy(self):
-        output = self.conn.read()
+        """
+        Read any available information from the game and call all of the
+        registered callbacks. It is the responsibility of the caller to make
+        sure that the call to `self.game.read()` will not block (e.g. by using
+        select or setting the fd to non-blocking mode)
+        """
+        output = self.game.read()
 
         for callback in self.callbacks:
             callback(output)
