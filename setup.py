8d119e4c7948fd8085f59412c3e6c8a4333581d0
Sam Gibson <sam@ifdown.net>
2011-12-06 16:48:17 +0530

    Fixing setup

--- setup.py
+++ setup.py
@@ -5,11 +5,12 @@ setup(
     version="0.3",
     author="Sam Gibson",
     author_email="sam@ifdown.net",
-    url="https://github.com/samfoo/noobhack",
+    url="https://samfoo.github.com/noobhack",
     description="noobhack helps you ascend at nethack",
     long_description=open("readme.md", "r").read(),
-    requires=["vt102 (>=0.3.2)"],
-    packages=["noobhack", "noobhack.game"],
+    install_requires=["vt102 >= 0.3.3"],
+    packages=["noobhack", "noobhack.game", "noobhack.ui"],
     scripts=["scripts/noobhack"],
+    keywords="nethack noobhack nao hack rogue",
     license="Lesser General Public License v3.0"
 )
